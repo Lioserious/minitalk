72ca7396fdeba75023ffdb8d00c35b7ffde9a6a2
Lioserious <lihrig@student.42heilbronn.de>
2025-03-17 17:36:20 +0100

    added char to biinnary function to cllient

--- client.c
+++ client.c
@@ -6,7 +6,26 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:10:24 by lihrig            #+#    #+#             */
-/*   Updated: 2025/03/17 16:10:24 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/17 17:35:45 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "minitalk.h"
+
+void character_to_binary(char t, char *binary)
+{
+	binary[8] = '\0';
+	int i;
+	int bit_value;
+	
+	i = 7;
+	while(i >= 0)
+	{
+		bit_value = (t >> i) & 1;
+		if(bit_value == 1)
+			binary[7-i] = '1';
+		else
+			binary[7-i] = '0';
+		i--;
+	} 
+}
