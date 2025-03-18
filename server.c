75b1cac5858c5bc801e6382f48633e450f24491b
Lioserious <lihrig@student.42heilbronn.de>
2025-03-18 15:13:25 +0100

    signal_to_output

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:10:16 by lihrig            #+#    #+#             */
-/*   Updated: 2025/03/18 14:54:30 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/18 15:12:39 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,25 @@ void print_message(char message[262144])
 }
 void signal_to_output(int sig, siginfo_t *info, void *context)
 {
+	static char bit = 0;
+	static int count = 0;
+	static int index = 0;
+	static char message[262144];
+	pid_t	client_pid;
+	
 	(void)context;
+	if(sig == SIGUSR1)
+		bit = (bit << 1) | 0;
+	else if(sig == SIGUSR2)
+		bit = (bit << 1) | 1;
+	count++;
+	if(count < 8)
+	{	
+		message[index] = bit;
+		index++;
+			if(message[index] == '\0')
+				print_message(message);
+		count = 0;
+		bit = 0;
+	}
 }
