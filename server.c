9411bda9e2adf307ba1305a462c6b8346a016ac1
Lioserious <lihrig@student.42heilbronn.de>
2025-03-18 14:55:23 +0100

    print message added

--- server.c
+++ server.c
@@ -6,12 +6,23 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:10:16 by lihrig            #+#    #+#             */
-/*   Updated: 2025/03/17 19:26:07 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/18 14:54:30 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minitalk.h"
 
+void print_message(char message[262144])
+{
+	int i;
+	
+	i = 0;
+	while(message[i] != NULL)
+	{
+		write(1, &message[i], 1);
+		i++;
+	}
+}
 void signal_to_output(int sig, siginfo_t *info, void *context)
 {
 	(void)context;
