ea9408da62a0c059c609d7a1135bb3bdaac5ae75
Lioserious <lihrig@student.42heilbronn.de>
2025-03-18 17:37:09 +0100

    bonus files

---
+++ client_bonus.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 16:10:24 by lihrig            #+#    #+#             */
+/*   Updated: 2025/03/18 17:19:49 by lihrig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+// SIGUSR 2 = 1
+// SIGUSR 1 = 0
+
+void	character_to_binary(char t, pid_t server_pid)
+{
+	int	i;
+	int	bit_value;
+
+	i = 7;
+	while (i >= 0)
+	{
+		bit_value = (t >> i) & 1;
+		if (bit_value == 1)
+			kill(server_pid, SIGUSR2);
+		else
+			kill(server_pid, SIGUSR1);
+		usleep(200);
+		i--;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	int		y;
+	pid_t	server_pid;
+
+	i = 0;
+	y = 2;
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s [server_pid] [message]\n");
+		exit(EXIT_FAILURE);
+	}
+	server_pid = ft_atoi(argv[1]);
+	if (server_pid <= 0)
+	{
+		ft_printf("Invalid PID\n");
+		exit(EXIT_FAILURE);
+	}
+	while (argv[2][i] != '\0')
+	{
+		character_to_binary(argv[2][i], server_pid);
+		i++;
+	}
+	return (character_to_binary('\0', server_pid), 0);
+}
---
+++ server_bonus.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 16:10:16 by lihrig            #+#    #+#             */
+/*   Updated: 2025/03/18 17:30:04 by lihrig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	print_message(char message[262144], int *index)
+{
+	int	i;
+
+	i = 0;
+	while (message[i] != '\0')
+	{
+		write(1, &message[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	i = 0;
+	while (i < 262144)
+		message[i++] = '\0';
+	*index = 0;
+}
+
+void	signal_to_output(int sig, siginfo_t *info, void *context)
+{
+	static char	bit = 0;
+	static int	count = 0;
+	static int	index = 0;
+	static char	message[262144];
+	pid_t		client_pid;
+
+	(void)context;
+	client_pid = info->si_pid;
+	if (sig == SIGUSR1)
+		bit = (bit << 1) | 0;
+	else if (sig == SIGUSR2)
+		bit = (bit << 1) | 1;
+	count++;
+	if (count == 8)
+	{
+		message[index] = bit;
+		if (message[index] == '\0')
+			print_message(message, &index);
+		else
+			index++;
+		count = 0;
+		bit = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sigact;
+	pid_t				pid;
+
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	sigact.sa_sigaction = signal_to_output;
+	sigact.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigemptyset(&sigact.sa_mask);
+	if (sigaction(SIGUSR1, &sigact, NULL) == -1)
+		sig_error();
+	if (sigaction(SIGUSR2, &sigact, NULL) == -1)
+		sig_error();
+	while (1)
+		pause();
+	return (0);
+}
