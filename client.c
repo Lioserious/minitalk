b750ffdbc405465707a0141ca3effad64a0837f8
Lioserious <lihrig@student.42heilbronn.de>
2025-03-18 16:38:20 +0100

    first working version segfaults when i sent the message

--- client.c
+++ client.c
@@ -6,41 +6,42 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:10:24 by lihrig            #+#    #+#             */
-/*   Updated: 2025/03/18 14:45:19 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/18 16:32:32 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minitalk.h"
+#include "minitalk.h"
 
-//SIGUSR 2 = 1
-//SIGUSR 1 = 0 
+// SIGUSR 2 = 1
+// SIGUSR 1 = 0
 
-void character_to_binary(char t, pid_t server_pid)
+void	character_to_binary(char t, pid_t server_pid)
 {
-	int i;
-	int bit_value;
-	
+	int	i;
+	int	bit_value;
+
 	i = 7;
-	while(i >= 0)
+	while (i >= 0)
 	{
 		bit_value = (t >> i) & 1;
-		if(bit_value == 1)
+		if (bit_value == 1)
 			kill(server_pid, SIGUSR2);
 		else
 			kill(server_pid, SIGUSR1);
 		usleep(200);
 		i--;
-	} 
+	}
 }
 
-int main(int argc, char* argv[])
+int	main(int argc, char *argv[])
 {
-	int i;
-	int y;
-	pid_t server_pid;
+	int		i;
+	int		y;
+	pid_t	server_pid;
 	
+	i = 0;
 	y = 2;
-	if (argc < 2 && argc < 3)
+	if (argc != 3)
 	{
 		ft_printf("Usage: %s [server_pid] [message]\n");
 		exit(EXIT_FAILURE);
@@ -51,18 +52,17 @@ int main(int argc, char* argv[])
 		ft_printf("Invalid PID\n");
 		exit(EXIT_FAILURE);
 	}
-
-	while(argv[y][i] != '\0')
+	while (argv[y][i] != '\0')
 	{
 		i = 0;
-		while(argv[y][i] != '\0')
+		while (argv[y][i] != '\0')
 		{
 			character_to_binary(argv[y][i], server_pid);
 			i++;
 		}
-		if(y < argc - 1)
+		if (y < argc - 1)
 			character_to_binary(' ', server_pid);
 		y++;
 	}
-	return(character_to_binary('\0', server_pid), 0);
+	return (character_to_binary('\0', server_pid), 0);
 }
--- server.c
+++ server.c
@@ -6,44 +6,70 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 16:10:16 by lihrig            #+#    #+#             */
-/*   Updated: 2025/03/18 15:12:39 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/03/18 16:34:12 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minitalk.h"
+#include "minitalk.h"
 
-void print_message(char message[262144])
+void	print_message(char message[262144], int *index)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
-	while(message[i] != NULL)
+	while (message[i] != '\0')
 	{
 		write(1, &message[i], 1);
 		i++;
 	}
+	write(1, "\n", 1);
+	*index = 0;
 }
-void signal_to_output(int sig, siginfo_t *info, void *context)
+void	signal_to_output(int sig, siginfo_t *info, void *context)
 {
-	static char bit = 0;
-	static int count = 0;
-	static int index = 0;
-	static char message[262144];
-	pid_t	client_pid;
-	
+	static char	bit = 0;
+	static int	count = 0;
+	static int	index = 0;
+	static char	message[262144];
+	pid_t		client_pid;
+
 	(void)context;
-	if(sig == SIGUSR1)
+	client_pid = info->si_pid;
+	if (sig == SIGUSR1)
 		bit = (bit << 1) | 0;
-	else if(sig == SIGUSR2)
+	else if (sig == SIGUSR2)
 		bit = (bit << 1) | 1;
 	count++;
-	if(count < 8)
-	{	
+	if (count == 8)
+	{
 		message[index] = bit;
 		index++;
-			if(message[index] == '\0')
-				print_message(message);
+		if (message[index] == '\0')
+			print_message(message, &index);
 		count = 0;
 		bit = 0;
 	}
+}
+void	sig_error(void)
+{
+	ft_printf("ERROR with Signal");
+	exit(EXIT_FAILURE);
+}
+int	main(void)
+{
+	struct sigaction	sigact;
+	pid_t				pid;
+
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	sigact.sa_sigaction = signal_to_output;
+	sigact.sa_flags = SA_SIGINFO | SA_RESTART;
+	sigemptyset(&sigact.sa_mask);
+	if (sigaction(SIGUSR1, &sigact, NULL) == -1)
+		sig_error();
+	if (sigaction(SIGUSR2, &sigact, NULL) == -1)
+		sig_error();
+	while (1)
+		pause();
+	return (0);
 }
